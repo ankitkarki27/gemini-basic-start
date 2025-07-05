9c4229c918482f72ada766700070bb5a9768afaf
ankitkarki27 <ankitkarki8088@gmail.com>
2025-07-06 02:53:52 +0545

    simple project on py

--- 01.py
+++ 01.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-# client = genai.Client(api_key="AIzaSyCfoJUvPh8GPoQ7hYhinr82qIR4gaooc9c")
 
 response= client.models.generate_content(
     model="gemini-2.5-pro",
